697072732d3dc961276a56e45b30e90faa84c53b
Trang Bui <buihoaib@gmail.com>
2023-10-03 10:48:06 +0700

    initial commit

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div
+            className="flex min-h-[10rem] border-t-2 items-center justify-center"
+        >
+            Footer
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+function Logo() {
+    return (
+        <div
+            className="absolute z-20 top-0 bg-transparent mt-8 ml-8"
+        >
+            <p
+                className="font-bold text-xl"
+            >
+                TrangBUI
+            </p>
+        </div>
+    )
+}
+
+export default Logo
