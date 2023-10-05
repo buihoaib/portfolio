5f4e81e0613568f85a69b30a2876e989dadac053
Trang Bui <buihoaib@gmail.com>
2023-10-05 23:58:24 +0700

    Changed footer UI

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,21 @@
+import { Heart } from "lucide-react"
+import SocialMediaFooter from "./social-media-footer"
+
 function Footer() {
     return (
         <div
-            className="flex min-h-[10rem] border-t-2 items-center justify-center"
+            className="flex flex-col border-t-2 items-center justify-center py-10 gap-4"
         >
-            Footer
-        </div>
+            <SocialMediaFooter />
+            <div className="flex flex-col justify-center items-center">
+                <p>&copy; 2023 All rights reserved.</p>
+                <p className="flex justify-center items-center">
+                    Developed with
+                    <span><Heart className="mx-1" size={16} fill="#FF0000" stroke="#FF0000" /></span>
+                    by Trang Bui
+                </p>
+            </div>
+        </div >
     )
 }
 
