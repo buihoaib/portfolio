2f0a0716058cfec124089687d228d87761376140
Trang Bui <buihoaib@gmail.com>
2023-10-06 13:46:38 +0700

    Added Hero section

---
+++ components/hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Download } from "lucide-react"
+import Image from "next/image";
+
+import { useSectionInView } from "@/hooks/use-section-in-view";
+import SocialMediaSide from "./social-media-side";
+import Link from "next/link";
+
+function Hero() {
+    const { ref } = useSectionInView("Home");
+
+    return (
+        <section
+            id="home"
+            ref={ref}
+            className="relative h-screen w-full bg-indigo-400 p-24 flex items-center justify-center gap-10"
+        >
+            <div className="flex flex-col gap-2 items-center">
+                <h1 className="font-bold text-5xl">Hello, I'm Trang</h1>
+                <p className="text-xl">I'm a Fullstack Software Developer</p>
+                <div className="flex  items-center">
+                    <Link href="/CV.pdf" className="flex gap-1 text-white">
+                        <Download size={20} />
+                        Download My CV
+                    </Link>
+                </div>
+                <SocialMediaSide />
+            </div>
+            <div className="font-bold p-10 border-2">
+                <h1>avatar</h1>
+                {/* <Image src={} /> */}
+            </div>
+        </section>
+    )
+}
+
+export default Hero
