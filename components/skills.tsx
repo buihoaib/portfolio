a117f0ae00caf32097ed68c5ba16b923a7016e0c
Trang Bui <buihoaib@gmail.com>
2023-10-10 15:32:08 +0700

    Updated responsive design for learning skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -68,13 +68,13 @@ function Skills() {
                     </ul>
                 </div>
 
-                <div className="flex flex-col items-center py-5 px-20 gap-2">
+                <div className="flex flex-col items-center py-5 px-2 md:px-10 lg:px-20 gap-2">
                     <p>I&apos;m currently learning: </p>
                     <ul className="flex gap-2 flex-wrap items-center justify-center">
                         {learningSkills.map((skill, index) => (
                             <motion.li
                                 key={index}
-                                className="flex flex-col items-center px-4 py-2 rounded-xl text-indigo-400 hover:bg"
+                                className="flex flex-col items-center px-2 sm:px-4 py-2 rounded-xl text-indigo-400 hover:bg"
                                 variants={fadeInAnimationVariants}
                                 initial="initial"
                                 whileInView="animate"
